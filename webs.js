4701c129f12c22f92aee184ceabdee29f63aa0b8
KooPages <245970062+KooPages@users.noreply.github.com>
2025-12-12 12:45:40 -0500

    Corregir descripciones genéricas en webs.js para mejorar la calidad de los datos.

--- webs.js
+++ webs.js
@@ -3,42 +3,42 @@
 const webs = [
     {
         title: "Suri Nails",
-        description: "Visitanos",
+        description: "Descubre los mejores productos y servicios.",
         url: "https://spoo.me/suri_nails",
         image: "https://github.com/KooPages/suri_nails/raw/refs/heads/main/logo.jpg",
         category: "Servicio"
     },
     {
         title: "Tienda Iyawo",
-        description: "Visitanos",
+        description: "Descubre los mejores productos y servicios.",
         url: "https://spoo.me/tienda_iyawo",
         image: "https://github.com/KooPages/tienda_iyawo/raw/refs/heads/main/logo.jpg",
         category: "Tienda"
     },
     {
         title: "Aly Salon",
-        description: "Visitanos",
+        description: "Descubre los mejores productos y servicios.",
         url: "https://spoo.me/aly_salon",
         image: "https://github.com/KooPages/aly_salon/raw/refs/heads/main/logo.jpg",
         category: "Servicio"
     },
     {
         title: "Carnicos Habana",
-        description: "Visitanos",
+        description: "Descubre los mejores productos y servicios.",
         url: "https://spoo.me/carnicos_habana",
         image: "https://github.com/KooPages/carnicos_habana/raw/refs/heads/main/logo.jpg",
         category: "Servicio"
     },
     {
         title: "Tienda De Todo Un Poco",
-        description: "Visitanos",
+        description: "Descubre los mejores productos y servicios.",
         url: "https://spoo.me/de_todo_un_poco",
         image: "https://github.com/KooPages/tienda_de_todo_un_poco/raw/refs/heads/main/logo.png",
         category: "Tienda"
     },
     {
         title: "Dulceria Lisst",
-        description: "Visitanos",
+        description: "Descubre los mejores productos y servicios.",
         url: "https://spoo.me/dulceria_lisset",
         image: "https://github.com/KooPages/dulceria_lisset/raw/refs/heads/main/logo.jpg",
         category: "Servicio"
